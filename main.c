62da62867a12f9e020a3e32d8a5aa57d69ae06ed
y-syo <mmoussou@student.42angouleme.fr>
2023-12-20 11:41:33 +0100

    init commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoussou <mmoussou@student.42angoulem      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 15:11:53 by mmoussou          #+#    #+#             */
+/*   Updated: 2023/12/18 19:40:27 by mmoussou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MacroLibX/includes/mlx.h"
+
+int	main(void)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 727, 727, "ft_seekers");
+	mlx_loop(mlx_ptr);
+
+}
